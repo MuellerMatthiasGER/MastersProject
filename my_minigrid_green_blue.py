01be710b83992e9dbf3d17980f3592b354b3cf03
Matthias Müller <matthias2.mueller@stud.uni-bamberg.de>
2023-07-04 18:46:42 +0100

    new green blue setup

--- my_minigrid_green_blue.py
+++ my_minigrid_green_blue.py
@@ -9,7 +9,7 @@ from gymnasium.envs.registration import register
 
 
 class Pickup(RoomGridLevel):
-    def __init__(self, target_idx=None, room_size=6, agent_view_size=3, **kwargs):
+    def __init__(self, target_color=None, target_type=None, room_size=6, agent_view_size=3, **kwargs):
         super().__init__(
             num_rows=1, num_cols=1, 
             room_size=room_size, 
@@ -17,7 +17,19 @@ class Pickup(RoomGridLevel):
             agent_view_size=agent_view_size, 
             **kwargs)
         self.objs = [Ball('green'), Ball('blue'), Box('green'), Box('blue')]
-        self.target_idx = target_idx
+        self.target_color = target_color
+        self.target_type = target_type
+
+        if target_type == 'ball':
+            if target_color == 'blue':
+                self.start_idx = 1
+            else:
+                self.start_idx = 0
+        else:
+            if target_color == 'blue':
+                self.start_idx = 3
+            else:
+                self.start_idx = 2
 
     def gen_mission(self):
         self.place_agent()
@@ -25,49 +37,69 @@ class Pickup(RoomGridLevel):
 
         num_objs = len(self.objs)
 
-        if self.target_idx is not None:
-            target_idx = self.target_idx
-        else:
-            target_idx = self._rand_int(0, num_objs)
-
         for i in range(num_objs):
-            idx = (target_idx + i) % num_objs
+            idx = (self.start_idx + i) % num_objs
             self.place_in_room(0, 0, self.objs[idx])
 
         self.check_objs_reachable()
-        target = self.objs[target_idx]
-        self.instrs = PickupInstr(ObjDesc(target.type, target.color), strict=True)
+        self.instrs = PickupInstr(ObjDesc(self.target_type, self.target_color), strict=True)
 
     def seed(self, random_seed):
         pass
 
+
+register(
+    id="MyMiniGrid-Pickup-v0",
+    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": None, "target_type": None},
+)
+
+register(
+    id="MyMiniGrid-PickupGreen-v0",
+    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": "green", "target_type": None},
+)
+
+register(
+    id="MyMiniGrid-PickupBlue-v0",
+    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": "blue", "target_type": None},
+)
+
+register(
+    id="MyMiniGrid-PickupBall-v0",
+    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": None, "target_type": "ball"},
+)
+
+register(
+    id="MyMiniGrid-PickupBox-v0",
+    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": None, "target_type": "box"},
+)
+
 register(
     id="MyMiniGrid-PickupGreenBall-v0",
     entry_point="my_minigrid_green_blue:Pickup",
-    kwargs={"target_idx": 0},
+    kwargs={"target_color": "green", "target_type": "ball"},
 )
 
 register(
     id="MyMiniGrid-PickupBlueBall-v0",
     entry_point="my_minigrid_green_blue:Pickup",
-    kwargs={"target_idx": 1},
+    kwargs={"target_color": "blue", "target_type": "ball"},
 )
 
 register(
     id="MyMiniGrid-PickupGreenBox-v0",
     entry_point="my_minigrid_green_blue:Pickup",
-    kwargs={"target_idx": 2},
+    kwargs={"target_color": "green", "target_type": "box"},
 )
 
 register(
     id="MyMiniGrid-PickupBlueBox-v0",
     entry_point="my_minigrid_green_blue:Pickup",
-    kwargs={"target_idx": 3},
-)
-
-register(
-    id="MyMiniGrid-PickupGreenBlue-v0",
-    entry_point="my_minigrid_green_blue:Pickup",
+    kwargs={"target_color": "blue", "target_type": "box"},
 )
 
 
