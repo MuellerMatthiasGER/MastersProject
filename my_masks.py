0db7069d92178f88c92273f3904d190c5c426e64
Matthias Müller <matthias2.mueller@stud.uni-bamberg.de>
2023-07-17 14:04:36 +0200

    concept masks, old masks are now binary

--- my_masks.py
+++ my_masks.py
@@ -6,6 +6,18 @@
 
 from deep_rl.mask_modules.mmn.mask_nets import GetSubnetContinuous, GetSubnetDiscrete, MultitaskMaskLinearSparse, mask_init, signed_constant
 from deep_rl.network.network_heads import *
+import torch.autograd as autograd
+
+class MyGetSubnetDiscrete(autograd.Function):
+    # only > instead of >=
+    @staticmethod
+    def forward(ctx, scores, a=0):
+        return (scores > a).float()
+
+    @staticmethod
+    def backward(ctx, g):
+        # send the gradient g straight-through on the backward pass.
+        return g
 
 class MyMultitaskMaskLinear(MultitaskMaskLinear):
     def __init__(self, *args, discrete=True, num_tasks=1, 
@@ -24,6 +36,9 @@ class MyMultitaskMaskLinear(MultitaskMaskLinear):
         )
         self.betas = nn.Parameter(torch.zeros(num_tasks, self.num_masks).type(torch.float32))
 
+        # subnet class
+        self._subnet_class = MyGetSubnetDiscrete
+
     def _forward_mask_linear_comb(self):
         _subnet = self.scores[self.num_masks_init - 1]
         
@@ -36,17 +51,11 @@ class MyMultitaskMaskLinear(MultitaskMaskLinear):
 
         # otherwise, a new task and it is not the first task. combine task mask with
         # masks from previous tasks.
-        _subnets = [self.scores[idx] for idx in range(self.num_masks_init - 1)]
+        _subnets = [self.scores[idx].detach() for idx in range(self.num_masks_init - 1)]
         
-        # scores of other masks can be modified but not change their sign, i.e. if a value is >= 0
-        clamped_subnets = []
-        for net in _subnets:
-            min = torch.where(net >= 0, torch.tensor(0.1), torch.tensor(-6))
-            max = torch.where(net < 0, torch.tensor(-0.1), torch.tensor(6))
-            clamped_subnets.append(net.clamp(min=min, max=max))
-        _subnets = clamped_subnets
+        _subnet = self.scores[self.num_masks_init - 1]
         _subnets.append(_subnet)
-
+        
         _betas = self.betas[self.task, 0:self.num_masks_init]
         _betas = self._subnet_class.apply(_betas)
         assert len(_betas) == len(_subnets), 'an error ocurred'
@@ -61,12 +70,11 @@ class MyMultitaskMaskLinear(MultitaskMaskLinear):
         # TODO: What if task is already known
 
         if self.task == 0:
-            mask = self._subnet_class.apply(self.scores[0]).type(torch.bool)
-            self.scores[0].data[~mask] = -0.1
+            self.scores[0].data = self._subnet_class.apply(self.scores[0])
             return
 
         _subnets = [self.scores[idx] for idx in range(self.num_masks_init - 1)]
-        existing_subnet_masks = [self._subnet_class.apply(net).type(torch.bool) for net in _subnets]
+        existing_subnet_masks = [net.type(torch.bool) for net in _subnets]
 
         _subnet = self.scores[self.num_masks_init - 1]
         _subnets.append(_subnet)
@@ -78,10 +86,12 @@ class MyMultitaskMaskLinear(MultitaskMaskLinear):
 
         # element wise sum of various masks
         _subnet_linear_comb = torch.stack(_subnets, dim=0).sum(dim=0)
-        final_mask = self._subnet_class.apply(_subnet_linear_comb).type(torch.bool)
+        new_scores = self._subnet_class.apply(_subnet_linear_comb)
+        self.scores[self.num_masks_init - 1].data = new_scores
+        final_mask = new_scores.type(torch.bool)
 
-        self.scores[self.num_masks_init - 1].data = _subnet_linear_comb.data
-        self.scores[self.num_masks_init - 1].data[~final_mask] = -0.1
+        # TODO: instead of setting new scores to 0 and 1, maybe use kaiming constant, 
+        # also do this at the top for task==0 
 
         # make new mask disjunct to existing masks
         for idx, mask in enumerate(existing_subnet_masks):
@@ -92,23 +102,35 @@ class MyMultitaskMaskLinear(MultitaskMaskLinear):
             # check if the new one is a subset of the old one
             if not (final_mask & ~mask).any():
                 # then remove new mask from old mask
-                self.scores[idx].data[final_mask] = -0.1
-                # TODO: add new betas
+                self.scores[idx].data[final_mask] = 0
+
+                # all older masks that use this mask must also use the new one to be complete
+                self.betas[self.betas[:, idx] > 0, self.num_masks_init - 1] = 0.5
             
             # check if the old one is a subset of the new one
             elif not (mask & ~final_mask).any():
                 # then remove old mask from new one
-                self.scores[self.num_masks_init - 1].data[mask] = -0.1
+                self.scores[self.num_masks_init - 1].data[mask] = 0
+
+                # ensure that old mask is used for this task
+                self.betas[self.task, idx] = 0.5
 
             else:
                 # remove both masks from each other and the intersection is the new one
-                self.scores[self.num_masks_init].data = _subnet_linear_comb
-                self.scores[self.num_masks_init].data[~final_mask | ~mask] = -0.1
+                self.scores[self.num_masks_init].data = new_scores
+                self.scores[self.num_masks_init].data[~final_mask | ~mask] = 0
 
-                self.scores[idx].data[final_mask] = -0.1
-                self.scores[self.num_masks_init - 1].data[mask] = -0.1
+                self.scores[idx].data[final_mask] = 0
+                self.scores[self.num_masks_init - 1].data[mask] = 0
 
+                # ensure right beta values
+                # all masks that use the old mask must use the intersection
+                self.betas[self.betas[:, idx] > 0, self.num_masks_init] = 0.5
+                # current task must use the old mask and the intersection
+                self.betas[self.task, [idx, self.num_masks_init]] = 0.5
+                
                 self.num_masks_init += 1
+
 
         
     def __repr__(self):
