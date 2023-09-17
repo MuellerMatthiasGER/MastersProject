fef351e69ee2c482704e1792e748a8c6f4c5820a
Matthias Müller <matthias2.mueller@stud.uni-bamberg.de>
2023-09-17 19:56:56 +0200

    minor change

--- my_experiments.py
+++ my_experiments.py
@@ -272,8 +272,5 @@ if __name__ == '__main__':
     set_one_thread()
     select_device(0) # -1 is CPU, a positive integer is the index of GPU
 
-    # env_config_path = "./env_configs/minigrid_color_shape.json"
-    # learn_color_shape(env_config_path)
-
     env_config_path = "env_configs/minigrid_green_blue.json"
     learn_green_blue(env_config_path)
